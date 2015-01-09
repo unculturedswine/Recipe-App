c2b59933d6e32da1bdb7e4bb3d8260b27bb90d0e
Joel Jenkins <jj@joeljenkins.me>
2015-01-08 20:12:46 -0700

    GitHub Steps 1 and 2 done and working okay.

--- Recipe App/RAAppDelegate.m
+++ Recipe App/RAAppDelegate.m
@@ -7,6 +7,7 @@
 //
 
 #import "RAAppDelegate.h"
+#import "RecipeViewController.h"
 
 @implementation RAAppDelegate
 
@@ -14,6 +15,16 @@
 {
     self.window = [[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]];
     // Override point for customization after application launch.
+    
+    // Start of Joel breaking things
+    
+    RecipeViewController *recipeViewController = [[RecipeViewController alloc] init];
+    UINavigationController *navController = [[UINavigationController alloc] initWithRootViewController:recipeViewController];
+    
+    self.window.rootViewController = navController;
+    
+    // End of Joel breaking things
+    
     self.window.backgroundColor = [UIColor whiteColor];
     [self.window makeKeyAndVisible];
     return YES;
---
+++ Recipe App/RecipeViewController.m
@@ -0,0 +1,42 @@
+//
+//  RecipeViewController.m
+//  Recipe App
+//
+//  Created by Joel on 1/8/15.
+//  Copyright (c) 2015 DevMountain. All rights reserved.
+//
+
+#import "RecipeViewController.h"
+
+@interface RecipeViewController ()
+
+@property (nonatomic, strong) UITableView *tableView;
+
+@end
+
+@implementation RecipeViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+    
+    self.tableView = [[UITableView alloc] initWithFrame:self.view.frame];
+    [self.view addSubview:self.tableView];
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
